42741db335f95d891cb951e9ac2c19dfa634c37f
StanisS <stanislav.i.stepanov@gmail.com>
2016-03-22 12:55:36 +0300

    Added a multi-project gradle config.

---
+++ home-page/src/main/java/org/stepanov/stan/MainClass.java
@@ -0,0 +1,11 @@
+package org.stepanov.stan;
+
+/**
+ * Created by Stepanov_SI
+ */
+public class MainClass {
+//    private final Logger logger = LoggerFactory.getLogger(org.stepanov.stan.MainClass.class);
+    public static void main(String[] args) {
+        System.out.println("Hello my gradle app.");
+    }
+}
